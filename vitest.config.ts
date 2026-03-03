90782641170ccaf0141b85f6f813755374c04157
Bruno <brunoariasalvescruz@gmail.com>
2026-03-03 13:45:08 -0300

    site portfolio

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "github.com/brunoccruzz/bruno-arias-alves-cruz", // 👈 IMPORTANTE
+
   plugins: [react()],
+
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
+
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
